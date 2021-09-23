aea41fc5c53d6fbbad46cafcf188b8bef52c61ad
Regex <56909681+RegexMC@users.noreply.github.com>
2021-09-23 18:17:06 +1000

    Fixed bug of manga not showing if no airing anime on list
    Haven't tested with airing anime on list as I don't have any lol
    
    Beforehand it was getting the manga section by index (which is 1 less if no anime airing)

--- src/pages/home.ts
+++ src/pages/home.ts
@@ -1,13 +1,51 @@
-import { elementListener, generateMangaEntry, getData } from "../utils/utils";
+import { getData, generateMangaEntry } from "../utils/utils";
 
-elementListener(document, "#app > div.page-content > div > div.home.full-width > div > div:nth-child(3)")
-	.then((element) => {
-		getData().then((data) => {
-			data.manga.forEach((m) => {
-				if (m.status == "Reading") {
-					(element.lastChild as Element).insertAdjacentHTML("beforeend", generateMangaEntry(m, "Home"));
-				}
-			});
+// Reworked to fix it not working if the user does not have any anime airing (selected third item, w/o airing manga is second item)
+// This seems to work (havent tested w/ anime airing as I don't have any) but seems inefficient.
+
+var qs = document.querySelector("#app > div.page-content > div > div.home.full-width > div > div:last-child");
+if (qs != null) {
+	getData().then((data) => {
+		data.manga.forEach((m) => {
+			if (m.status == "Reading") {
+				(qs?.lastChild as Element).insertAdjacentHTML("beforeend", generateMangaEntry(m, "Home"));
+			}
 		});
-	})
-	.catch((err) => console.log);
+	});
+}
+
+var observer = new MutationObserver((mutations) => {
+	mutations.forEach((mutation) => {
+		if (!mutation.addedNodes) return;
+		for (let i = 0; i < mutation.addedNodes.length; i++) {
+			try {
+				const node = mutation.addedNodes[i];
+				if (node.nodeType == 1) {
+					if ((node as Element).className == "list-preview-wrap") {
+						if ((node.firstChild?.firstChild as any).innerText == "Manga in Progress") {
+							var element = node as Element;
+							getData().then((data) => {
+								data.manga.forEach((m) => {
+									if (m.status == "Reading") {
+										(element?.lastChild as Element).insertAdjacentHTML(
+											"beforeend",
+											generateMangaEntry(m, "Home")
+										);
+									}
+								});
+							});
+							observer.disconnect();
+						}
+					}
+				}
+			} catch (e) {}
+		}
+	});
+});
+
+observer.observe(document.body, {
+	childList: true,
+	subtree: true,
+	attributes: false,
+	characterData: false
+});
